5e0f7b8e0490b96cd998682600b097e702d6fba0
jtlewis81 <jtlewis91481@gmail.com>
2022-08-26 17:00:32 -0400

    Activity 4 initial commit

---
+++ Topic 2 - Activity 4/Activity4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Activity4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox_seconds = new System.Windows.Forms.TextBox();
+			this.label_output = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(58, 106);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(284, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Enter the Number of Seconds Elapsed:";
+			// 
+			// textBox_seconds
+			// 
+			this.textBox_seconds.Location = new System.Drawing.Point(364, 103);
+			this.textBox_seconds.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox_seconds.Name = "textBox_seconds";
+			this.textBox_seconds.Size = new System.Drawing.Size(179, 26);
+			this.textBox_seconds.TabIndex = 1;
+			this.textBox_seconds.TextChanged += new System.EventHandler(this.UpdateTime);
+			// 
+			// label_output
+			// 
+			this.label_output.AutoSize = true;
+			this.label_output.Location = new System.Drawing.Point(157, 181);
+			this.label_output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_output.Name = "label_output";
+			this.label_output.Size = new System.Drawing.Size(291, 22);
+			this.label_output.TabIndex = 2;
+			this.label_output.Text = "0 Days : 0 Hours : 0 Minutes : 0 Seconds";
+			this.label_output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.label2.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label2.Location = new System.Drawing.Point(218, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(165, 27);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Time Calculator";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ClientSize = new System.Drawing.Size(593, 305);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label_output);
+			this.Controls.Add(this.textBox_seconds);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Form1";
+			this.Text = "Activity 4";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox_seconds;
+		private System.Windows.Forms.Label label_output;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Topic 2 - Activity 4/Activity4/Form1.cs
@@ -0,0 +1,89 @@
+﻿// Jamie Lewis
+// CST-150
+// 8/26/22
+// The modifications to this auto-generated file are my own work.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity4
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+		// Runs everytime the textbox input is changed
+		private void UpdateTime(object sender, EventArgs e)
+		{
+			// declare a variable to store the user input into
+			int time = 0;
+
+			//check if the input is blank
+			if (textBox_seconds.Text != "")
+			{
+				// validate input
+				if (int.TryParse(textBox_seconds.Text, out time))
+				{
+					// do not process data here because it causes a bug where the
+					// input field holds the last digit when trying to clear it
+				}
+				else
+				{
+					// display error on invalid input, such as letters
+					MessageBox.Show("Please enter numerical digits only!");
+					// clear the input when the user closes the message box
+					textBox_seconds.Text = "";
+				}
+			}
+
+			// declare variables for the time breakdown
+			int minutes = 0;
+			int hours = 0;
+			int days = 0;
+
+			// calculate days and update time
+			days = time / 86400;
+			time -= days * 86400;
+
+			// claculate hours and update time
+			hours = time / 3600;
+			time -= hours * 3600;
+
+			// calculate minutes and update time
+			minutes = time / 60;
+			time -= minutes * 60;
+
+			// update the label representing the output based on the user's input
+			label_output.Text = days + " " + GetPlurality("Day", days) + " : " +
+							hours + " " + GetPlurality("Hour", hours) + " : " +
+							minutes + " " + GetPlurality("Minute", minutes) + " : " +
+							time + " " + GetPlurality("Second", time);
+		}
+
+		// bonus helper method that updates the label with singular vs plural wording
+		private string GetPlurality(string timeUnit, int num)
+		{
+			if(num == 1)
+			{
+				// if the value of a time unit is equal to 1 and only 1, return it as passed
+				return timeUnit;
+			}
+			else
+			{
+				// if the value is 0 or more than 1, add an "s"
+				return timeUnit + "s";
+			}
+		}
+	}
+}
