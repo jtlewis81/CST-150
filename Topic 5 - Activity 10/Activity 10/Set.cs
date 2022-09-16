5838274d3b44611af32e61c606fe0640c7a36935
jtlewis81 <jtlewis91481@gmail.com>
2022-09-16 10:54:25 -0400

    Activity 9 & 10 initial commit

---
+++ Topic 5 - Activity 10/Activity 10/Set.cs
@@ -0,0 +1,102 @@
+﻿// Jamie Lewis
+// CST-150
+// 9/16/22
+// The comments and corrections made on this copied file are my own work.
+// The code was copied from the assignment directions as instructed.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Lydia's code - find the errors!
+namespace CST117_IC08_console
+{
+     // Updated method naming convention to standards (names should begin with a capital letter).
+
+     class Set
+     {
+          private List<int> elements;
+
+
+          public Set()
+          {
+               elements = new List<int>();
+          }
+
+          public bool AddElement(int val)
+          {
+               if (ContainsElement(val)) return false;
+               else
+               {
+                    elements.Add(val);
+                    return true;
+               }
+          }
+
+          // Bug #1
+          // The 'i' in "i++' in the for loop initialization was underlined.
+          // The popup tip on mouse hovering indicated that it was unreachable code.
+          // This indicates that the loop will exit before any iterating is done.
+          // In other words, it was only checking the first element in the Set.
+          private bool ContainsElement(int val)
+          {
+               for (int i = 0; i < elements.Count; i++)
+               {
+                    if (val == elements[i])
+                         return true;
+
+                    // The solution here is to remove the "else return false;" lines from inside the loop.
+                    // The method will return false after the for loop is completed if it does not return true first.
+                    
+                    //else
+                    //     return false;
+               }
+               return false;
+          } 
+
+          public override string ToString()
+          {
+               string str = "";
+               foreach (int i in elements)
+               {
+                    str += i + " ";
+               }
+               return str;
+          }
+
+          public void ClearSet()
+          {
+               elements.Clear();
+          }
+
+          // Bug #2
+          // This Union method modifies the calling Set.
+          // Bug #3
+          // The passed parameter Set is returned insead of the combined elements.
+
+          public Set Union(Set rhs)
+          {
+               // add temp Set variable
+               Set temp = new Set();
+
+               // copy this set
+               for(int i = 0; i < this.elements.Count; i++)
+			{
+                    temp.AddElement(this.elements[i]);
+			}
+
+               for (int i = 0; i < rhs.elements.Count; i++)
+               {
+                    // Add elements of the passed Set (rhs) to the temp Set variable instead of to the  original Set
+                    // this.AddElement(rhs.elements[i]);
+                    temp.AddElement(rhs.elements[i]);
+               }
+
+               // return the temp Set instead of the passed Set.
+               // return rhs;
+               return temp;
+          }
+     }
+}
---
+++ Topic 5 - Activity 9/Activity 9/Program.cs
@@ -0,0 +1,65 @@
+﻿// Jamie Lewis
+// CST-150
+// 9/16/22
+// This is my own work.
+
+// Instructions state that methods need not be implemented,
+// but I added default return values where necessasry in order to eliminate errors.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_9
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+
+	// Write a class that contains the following “stubs” for methods used in measurement conversion
+	// and include a screenshot of your program running with successful output.
+	public class Activity9
+	{
+		// 1. Write a method that takes two integers and displays their sum.
+		public void ShowSum(int intA, int intB) { }
+
+		// 2. Write a method that takes five doubles and returns their average.
+		public double GetAverageDouble(double doubleA, double doubleB, double doubleC, double doubleD, double doubleE) { return 0; }
+
+		// 3. Write a method that returns the sum of two randomly generated integers.
+		public int GetSumOfRandomInts() { return 0; }
+
+		// 4. Write a method that takes three integers and returns true if their sum is divisible by 3, false otherwise.
+		public bool IsSumDivisibleByThree(int intA, int intB, int intC) { return false; }
+
+		// 5. Write a method that takes two strings and displays the string that has fewer characters.
+		public void ShowShortestString(string stringA, string stringB) { }
+
+		// 6. Write a method that takes an array of doubles and returns the largest value in the array.
+		public double GetLargestDoubleFromArray(double[] doublesArray) { return 0; }
+
+		// 7. Write a method that generates and returns an array of fifty integer values.
+		public int[] GenerateArrayOfFiftyInts() { return new int[50]; }
+
+		// 8. Write a method that takes two bool variables and returns true if they have the same value, false otherwise.
+		public bool BoolsHaveSameValue(bool boolA, bool boolB) { return false; }
+
+		// 9. Write a method that takes an int and a double and returns their product.
+		public double GetIntTimesDouble(int intNum, double doubleNum) { return 0; }
+
+		// 10. Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+		public int GetAverageOfIntMatrix(int[][] intMatrix) { return 0; }
+	}
+}
