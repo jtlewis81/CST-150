989ba1ceb3a08cf074a498f09193a661325f8430
jtlewis81 <jtlewis91481@gmail.com>
2022-09-24 01:06:21 -0400

    Activity 13 initial commit

---
+++ Topic 6 - Activity 13/Activity13/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Activity13
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.exit_button = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.space_label_0 = new System.Windows.Forms.Label();
+			this.space_label_1 = new System.Windows.Forms.Label();
+			this.space_label_2 = new System.Windows.Forms.Label();
+			this.space_label_3 = new System.Windows.Forms.Label();
+			this.space_label_4 = new System.Windows.Forms.Label();
+			this.space_label_5 = new System.Windows.Forms.Label();
+			this.space_label_6 = new System.Windows.Forms.Label();
+			this.space_label_7 = new System.Windows.Forms.Label();
+			this.space_label_8 = new System.Windows.Forms.Label();
+			this.status_label = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// exit_button
+			// 
+			this.exit_button.BackColor = System.Drawing.Color.Silver;
+			this.exit_button.Location = new System.Drawing.Point(30, 466);
+			this.exit_button.Name = "exit_button";
+			this.exit_button.Size = new System.Drawing.Size(169, 41);
+			this.exit_button.TabIndex = 2;
+			this.exit_button.Text = "&New Game";
+			this.exit_button.UseVisualStyleBackColor = false;
+			this.exit_button.Click += new System.EventHandler(this.StartNewGame);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Silver;
+			this.button1.Location = new System.Drawing.Point(222, 466);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(180, 41);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "E&xit";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Exit);
+			// 
+			// space_label_0
+			// 
+			this.space_label_0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_0.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_0.Location = new System.Drawing.Point(28, 26);
+			this.space_label_0.Name = "space_label_0";
+			this.space_label_0.Size = new System.Drawing.Size(100, 100);
+			this.space_label_0.TabIndex = 0;
+			this.space_label_0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_1
+			// 
+			this.space_label_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_1.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_1.Location = new System.Drawing.Point(165, 26);
+			this.space_label_1.Name = "space_label_1";
+			this.space_label_1.Size = new System.Drawing.Size(100, 100);
+			this.space_label_1.TabIndex = 4;
+			this.space_label_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_2
+			// 
+			this.space_label_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_2.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_2.Location = new System.Drawing.Point(302, 26);
+			this.space_label_2.Name = "space_label_2";
+			this.space_label_2.Size = new System.Drawing.Size(100, 100);
+			this.space_label_2.TabIndex = 5;
+			this.space_label_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_3
+			// 
+			this.space_label_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_3.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_3.Location = new System.Drawing.Point(28, 149);
+			this.space_label_3.Name = "space_label_3";
+			this.space_label_3.Size = new System.Drawing.Size(100, 100);
+			this.space_label_3.TabIndex = 6;
+			this.space_label_3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_4
+			// 
+			this.space_label_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_4.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_4.Location = new System.Drawing.Point(165, 149);
+			this.space_label_4.Name = "space_label_4";
+			this.space_label_4.Size = new System.Drawing.Size(100, 100);
+			this.space_label_4.TabIndex = 7;
+			this.space_label_4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_5
+			// 
+			this.space_label_5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_5.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_5.Location = new System.Drawing.Point(302, 149);
+			this.space_label_5.Name = "space_label_5";
+			this.space_label_5.Size = new System.Drawing.Size(100, 100);
+			this.space_label_5.TabIndex = 8;
+			this.space_label_5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_6
+			// 
+			this.space_label_6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_6.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_6.Location = new System.Drawing.Point(28, 275);
+			this.space_label_6.Name = "space_label_6";
+			this.space_label_6.Size = new System.Drawing.Size(100, 100);
+			this.space_label_6.TabIndex = 9;
+			this.space_label_6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_7
+			// 
+			this.space_label_7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_7.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_7.Location = new System.Drawing.Point(165, 275);
+			this.space_label_7.Name = "space_label_7";
+			this.space_label_7.Size = new System.Drawing.Size(100, 100);
+			this.space_label_7.TabIndex = 10;
+			this.space_label_7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// space_label_8
+			// 
+			this.space_label_8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.space_label_8.Font = new System.Drawing.Font("Tahoma", 64F);
+			this.space_label_8.Location = new System.Drawing.Point(302, 275);
+			this.space_label_8.Name = "space_label_8";
+			this.space_label_8.Size = new System.Drawing.Size(100, 100);
+			this.space_label_8.TabIndex = 11;
+			this.space_label_8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// status_label
+			// 
+			this.status_label.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.status_label.Location = new System.Drawing.Point(30, 402);
+			this.status_label.Name = "status_label";
+			this.status_label.Size = new System.Drawing.Size(372, 37);
+			this.status_label.TabIndex = 12;
+			this.status_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Green;
+			this.ClientSize = new System.Drawing.Size(431, 535);
+			this.Controls.Add(this.status_label);
+			this.Controls.Add(this.space_label_8);
+			this.Controls.Add(this.space_label_7);
+			this.Controls.Add(this.space_label_6);
+			this.Controls.Add(this.space_label_5);
+			this.Controls.Add(this.space_label_4);
+			this.Controls.Add(this.space_label_3);
+			this.Controls.Add(this.space_label_2);
+			this.Controls.Add(this.space_label_1);
+			this.Controls.Add(this.space_label_0);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.exit_button);
+			this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Activity13";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button exit_button;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label space_label_0;
+		private System.Windows.Forms.Label space_label_1;
+		private System.Windows.Forms.Label space_label_2;
+		private System.Windows.Forms.Label space_label_3;
+		private System.Windows.Forms.Label space_label_4;
+		private System.Windows.Forms.Label space_label_5;
+		private System.Windows.Forms.Label space_label_6;
+		private System.Windows.Forms.Label space_label_7;
+		private System.Windows.Forms.Label space_label_8;
+		private System.Windows.Forms.Label status_label;
+	}
+}
+
---
+++ Topic 6 - Activity 13/Activity13/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13
+{
+	public partial class Form1 : Form
+	{
+		// declare "permanent" game variables (they are the same for every game and never change)
+		Label[,] gridSpaces;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			// initialize permanent game variables
+			gridSpaces = new Label[,] { { space_label_0, space_label_1, space_label_2 }, { space_label_3, space_label_4, space_label_5 }, { space_label_6, space_label_7, space_label_8 } };
+		}
+
+		private void StartNewGame(object sender, EventArgs e)
+		{
+			ResetSpaces();
+
+			// declare and initialize "fluid" game variables (they get reset for every new game)
+			bool isPlayer1Turn = true;
+			List<int[]> availableGridSpaces = new List<int[]>();
+			availableGridSpaces.Add(new int[] { 0, 0 });
+			availableGridSpaces.Add(new int[] { 0, 1 });
+			availableGridSpaces.Add(new int[] { 0, 2 });
+			availableGridSpaces.Add(new int[] { 1, 0 });
+			availableGridSpaces.Add(new int[] { 1, 1 });
+			availableGridSpaces.Add(new int[] { 1, 2 });
+			availableGridSpaces.Add(new int[] { 2, 0 });
+			availableGridSpaces.Add(new int[] { 2, 1 });
+			availableGridSpaces.Add(new int[] { 2, 2 });
+
+			// main game loop
+			for (int turn = 1; turn < 10; turn++)
+			{
+				// pick a random remaining grid space using a list of remaining spaces
+				int index = new Random().Next(0, availableGridSpaces.Count);
+				int[] nextPlayedSpace = availableGridSpaces[index];
+
+				// use 2d array index to set player marker
+				if (isPlayer1Turn)
+				{
+					gridSpaces[nextPlayedSpace[0], nextPlayedSpace[1]].Text = "X";
+				}
+				else if(!isPlayer1Turn)
+				{
+					gridSpaces[nextPlayedSpace[0], nextPlayedSpace[1]].Text = "O";
+				}
+
+				// check if game over
+				if (turn >= 3 && CheckForWinner(isPlayer1Turn))
+				{
+					break;
+				}
+				else if(turn == 9)
+				{
+					status_label.Text = "Draw!";
+					break;
+				}
+
+				// remove played space from available spaces list
+				availableGridSpaces.Remove(nextPlayedSpace);
+
+
+				// change players turn if game will continue
+				isPlayer1Turn = !isPlayer1Turn;
+			}
+		}
+
+		private bool CheckForWinner(bool isPlayer1Turn)
+		{
+			// check columns, rows, and diagonals that are not empty
+			if ( gridSpaces[0, 0].Text.Equals(gridSpaces[0, 1].Text) && gridSpaces[0, 1].Text.Equals(gridSpaces[0, 2].Text) && !gridSpaces[0, 0].Text.Equals("") ||
+				gridSpaces[1, 0].Text.Equals(gridSpaces[1, 1].Text) && gridSpaces[1, 1].Text.Equals(gridSpaces[1, 2].Text) && !gridSpaces[1, 0].Text.Equals("") ||
+				gridSpaces[2, 0].Text.Equals(gridSpaces[2, 1].Text) && gridSpaces[2, 1].Text.Equals(gridSpaces[2, 2].Text) && !gridSpaces[2, 0].Text.Equals("") ||
+				gridSpaces[0, 0].Text.Equals(gridSpaces[1, 0].Text) && gridSpaces[1, 0].Text.Equals(gridSpaces[2, 0].Text) && !gridSpaces[0, 0].Text.Equals("") ||
+				gridSpaces[0, 1].Text.Equals(gridSpaces[1, 1].Text) && gridSpaces[1, 1].Text.Equals(gridSpaces[2, 1].Text) && !gridSpaces[0, 1].Text.Equals("") ||
+				gridSpaces[0, 2].Text.Equals(gridSpaces[1, 2].Text) && gridSpaces[1, 2].Text.Equals(gridSpaces[2, 2].Text) && !gridSpaces[0, 2].Text.Equals("") ||
+				gridSpaces[0, 0].Text.Equals(gridSpaces[1, 1].Text) && gridSpaces[1, 1].Text.Equals(gridSpaces[2, 2].Text) && !gridSpaces[0, 0].Text.Equals("") ||
+				gridSpaces[0, 2].Text.Equals(gridSpaces[1, 1].Text) && gridSpaces[1, 1].Text.Equals(gridSpaces[2, 0].Text) && !gridSpaces[0, 2].Text.Equals(""))
+			{
+				// update gaem status if game over
+				if (isPlayer1Turn)
+				{
+					status_label.Text = "Player 1 Wins!";
+				}
+				else
+				{
+					status_label.Text = "Player 2 Wins!";
+				}
+
+				return true;
+			}
+			// no winner
+			else
+			{
+				return false;
+			}			
+		}
+
+		void ResetSpaces()
+		{
+			foreach (Label space in gridSpaces)
+			{
+				space.Text = "";
+			}
+		}
+
+		private void Exit(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
